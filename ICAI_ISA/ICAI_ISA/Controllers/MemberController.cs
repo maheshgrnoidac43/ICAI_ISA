c74e1e97eee6ebc5ac66b0164679e4023b1fdd44
maheshgrnoidac43 <maheshgrnoidac43@gmail.com>
2023-06-29 13:08:27 +0530

    Created New Page for Search Isa No.

---
+++ ICAI_ISA/ICAI_ISA/Controllers/MemberController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ICAI_ISA.Controllers
+{
+    public class MemberController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public MemberController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public async Task<IActionResult> SearchIsaNo()
+        {            
+            return View();
+        }
+    }
+}
