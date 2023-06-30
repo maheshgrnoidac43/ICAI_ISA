a036e2dbbd165c4e714a3893dbb546594cc49dbd
maheshgrnoidac43 <maheshgrnoidac43@gmail.com>
2023-06-30 12:13:12 +0530

    Add Models for ISA Search Number in VIP Data

---
+++ ICAI_ISA/ICAI_ISA.Model/SearchIsaNo.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ICAI_ISA.Model
+{
+    public class SearchIsaNo
+    {
+        public string MemberNo { get; set; }
+        public string Dob { get; set; }
+    }
+}
